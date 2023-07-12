7a21c6d33f2e8eb13ac4746432bc362562c10439
Ivan <90147664+Brossend@users.noreply.github.com>
2023-07-12 22:23:51 +0600

    Creating an article card, a drop-down window for complaints and mock data
    An article card has been made for all sections, a drop-down menu for complaints has been made, mock data for the article card has been made and processed

---
+++ src/components/mainpage/dropdown/Dropdown.tsx
@@ -0,0 +1,37 @@
+import { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Dropdown.module.css';
+
+import reportIcon from '../../../img/article/fi-rs-triangle.svg';
+
+export function Dropdown({ setDropdown }: { setDropdown: () => void }) {
+  //Сохранение объекта с dropdown меню
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  //Отслеживание клика вне dropdown меню и изменение его состояния
+  useEffect(() => {
+    const handleClick = (event: any): void => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdown();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
+
+  return (
+    /* Dropdown меню */
+
+    <div ref={dropdownRef} className={styles.dropdown}>
+      <Link className={styles.dropdownButton} to="/">
+        <img className={styles.dropdownImage} src={reportIcon} alt="record" />
+        Пожаловаться
+      </Link>
+    </div>
+  );
+}
