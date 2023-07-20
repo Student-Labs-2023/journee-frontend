abe1bd27bbb968cba89c234a2ce9afb7b92fcfa2
Ivan <90147664+Brossend@users.noreply.github.com>
2023-07-20 22:04:05 +0600

    Profile of a user and another user. Interface fixes, drop-down menu for a user and another user's profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { PopularPage } from './pages/PopularPage';
 import { FreshPage } from './pages/FreshPage';
 import { RoutesPage } from './pages/RoutesPage';
 import { MainLayout } from './layouts/MainLayout';
+import {ProfilePage} from "./pages/ProfilePage";
+import {ProfileLayout} from "./layouts/ProfileLayout";
+import {OtherUserPage} from "./pages/OtherUserPage";
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
           <Route index element={<PopularPage />} />
           <Route path="/fresh" element={<FreshPage />} />
           <Route path="/routes" element={<RoutesPage />} />
+        </Route>
+        <Route path="/profile" element={<ProfileLayout />}>
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/other" element={<OtherUserPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
