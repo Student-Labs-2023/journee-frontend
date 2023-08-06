eb9a30fb007656152b2187ca6e5a1125a96da209
Ivan <90147664+Brossend@users.noreply.github.com>
2023-08-06 22:13:51 +0600

    Добавление скролла статей в блоке посередине экрана

--- src/pages/popularPage/PopularPage.tsx
+++ src/pages/popularPage/PopularPage.tsx
@@ -1,20 +1,22 @@
-import {NavLink, Link} from 'react-router-dom';
-
 import {ArticleCard} from '../../components/mainpage/ArticleCard';
 
 import styles from './PopularPage.module.css';
 
 import {get as get_articles} from '../../data/Article';
-import { useEffect, useState } from 'react';
-import { SideBar } from '../../components/sidebar/SideBar';
+import {useEffect, useState} from 'react';
+import {SideBar} from '../../components/sidebar/SideBar';
 
 export function PopularPage() {
+    let height: any;
+
     const [Articles, setArticels] = useState([])
     useEffect(() => {
         get_articles().then(res => {
             setArticels(res)
         })
     }, [])
+
+
     return (
         // Главная страница
         <div key="PopularPage" className={`columns ${styles.main}`}>
@@ -22,15 +24,18 @@ export function PopularPage() {
             {/* Медиа */}
 
             <div className="column is-2">
-                <SideBar />
+                <SideBar/>
             </div>
             {/* Подгрузка карточек со статьями */}
 
-            <div className="column is-6">
-                {Articles.map((prop) => (
-                    <ArticleCard props={prop}/>
-                ))}
+            <div className={`column is-6 ${styles.scroll}`}>
+                <div>
+                    {Articles.map((prop) => (
+                        <ArticleCard props={prop}/>
+                    ))}
+                </div>
             </div>
+
             <div className="column is-2"></div>
             <div className="column is-1"></div>
         </div>
